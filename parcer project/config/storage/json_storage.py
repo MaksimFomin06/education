98b29588e85773f0ae41803e5a9666bbd1630df7
Maks <mr.maksim06@mail.ru>
2026-03-16 10:34:27 +0300

    Сделал сохранение по определенному пути

--- parcer project/config/storage/json_storage.py
+++ parcer project/config/storage/json_storage.py
@@ -1,12 +1,20 @@
 from dataclasses import asdict
 import json
 from config.models import Config
+import os
 
 
 class JSONStorage:
-    @staticmethod
-    def save(config: Config, filepath: str) -> None:
+    def __init__(self, base_dir: str = "config/configs"):
+        self.base_dir = base_dir
+
+        if not os.path.exists(self.base_dir):
+            os.makedirs(self.base_dir)
+
+    def save(self, config: Config, filename: str) -> None:
         config_to_dct = asdict(config)
+
+        filepath = os.path.join(self.base_dir, filename)
 
         with open(filepath, "w", encoding="utf-8") as file:
             json.dump(config_to_dct, file, indent=4, ensure_ascii=False)
